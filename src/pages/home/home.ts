2745287d9ecfb41a9ea19803ef6ab8e206be8a98
erginipekci7 <erginipekci7@gmail.com>
2017-10-21 03:34:39 +0300

    navCtrl ile push işlemini gerçekleştirdik.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FirstPage } from '../first/first';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,13 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+  /**
+   * home.html 'deki button ile openFirstPage() fonksiyonunu cagiriyoruz.
+   * navCtrl ile sayfaya yonlendiriyoruz.
+   */
+  openFirstPage(){
+    this.navCtrl.push(FirstPage)
   }
 
 }
